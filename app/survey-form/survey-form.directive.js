ec207a88de2e435348aaf6008522d077294d8bfa
Adi Vichter <adivich@gmail.com>
2017-12-15 15:41:41 +0200

    css + using survey-form as a directive

--- app/survey-form/survey-form.directive.js
+++ app/survey-form/survey-form.directive.js
@@ -1,14 +1,20 @@
 'use strict';
 
 angular.module('survey-form.module.js')
-    .component('surveyForm', {
+.directive('surveyForm', function () {
+    return {
+        restrict: 'EA',
         templateUrl: 'survey-form/survey-form.template.html',
         controller: ['$routeParams','surveysListData', function SurveyFormController($routeParams, surveysListData) {
-            var index = parseInt($routeParams.surveysId)
+            var index = parseInt($routeParams.surveysId);
 
             this.survey = surveysListData[index];
             this.questionIndex = 0;
             this.currentQuestion = this.survey.questions[this.questionIndex];
 
-        }]
-    });
+        }],
+        controllerAs: 'surveyForm',
+        bindToController: true
+
+    };
+});
